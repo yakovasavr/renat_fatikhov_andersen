dd37fc183dced701558b08750ac134b410b2f82f
Renat Fatikhov <Renat.Fatikhov@icl-services.com>
2021-11-08 18:11:30 +0300

    first

---
+++ containsDuplicate/containsDuplicate.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func containsDuplicate(nums []int) bool {
+	for i, value := range nums {
+		j := i + 1
+		for j < len(nums) {
+			if value == nums[j] {
+				return true
+			}
+			j++
+		}
+	}
+	return false
+}
+
+func main() {
+	arr := []int{1, 2, 3, 4, 2}
+	fmt.Println(containsDuplicate(arr))
+}
